464f6bd59f4bdb487653809efb2419b89126b82b
oidro <31112680+oidro@users.noreply.github.com>
2022-04-06 18:33:24 -0400

    Remove C++ binding/compilation files
    Keep our C++ stuff for reference while porting.

--- sourcecode/windowsCompileCPPBindings.bat
+++
@@ -1,3 +0,0 @@
-cd godot-cpp
-scons platform=windows generate_bindings=yes use_custom_api_file=yes custom_api_file=../api.json bits=64 target=release -j4
-cd ..
--- sourcecode/windowsCompileLibrary.bat
+++
@@ -1,6 +0,0 @@
-@ECHO off
-echo If using MSVC, make sure you're in the x64 Native Tools command prompt!
-
-REM Using multiple threads for compilation on Windows seems
-REM to cause link errors, for some reason.
-scons platform=windows bits=64 target=release -j1
