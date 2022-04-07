0a70de74d9ebfdde2af6dd9a9a68189ef43e10e6
Cherry <staticdroidpop@gmail.com>
2022-04-06 22:03:52 -0400

    C++ bindings

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 This is a very early development of a "voxel based Animal Crossing" sandbox game.
 
 # Things of Importance
-- Project version: 3.4.4
-- C++ bindings version: 3.4.3
+- Project version: 4.0 - Alpha 6
+- C++ bindings version: [fad357](https://github.com/godotengine/godot-cpp/tree/fad357bbe6a8fbc105b383e29c5c16b188e0fd20)
 
   You will have to compile the Godot-CPP bindings and then the chunk generation library.
   Sources included already and an easy build script is provided!
--- sourcecode/godot-cpp/misc/scripts/clang_format.sh
+++ sourcecode/godot-cpp/misc/scripts/clang_format.sh
@@ -1,43 +1,39 @@
 #!/usr/bin/env bash
 
-# This script runs clang-format on all relevant files in the repo.
+# This script runs clang-format and fixes copyright headers on all relevant files in the repo.
 # This is the primary script responsible for fixing style violations.
 
 set -uo pipefail
-IFS=$'\n\t'
 
-CLANG_FORMAT_FILE_EXTS=(".c" ".h" ".cpp" ".hpp" ".cc" ".hh" ".cxx" ".m" ".mm" ".inc" ".java" ".glsl")
+# Loops through all code files tracked by Git.
+git ls-files -- '*.c' '*.h' '*.cpp' '*.hpp' '*.cc' '*.hh' '*.cxx' '*.m' '*.mm' '*.inc' '*.java' '*.glsl' \
+                ':!:.git/*' ':!:thirdparty/*' ':!:platform/android/java/lib/src/com/google/*' ':!:*-so_wrap.*' |
+while read -r f; do
+    # Run clang-format.
+    clang-format --Wno-error=unknown -i "$f"
 
-# Loops through all text files tracked by Git.
-git grep -zIl '' |
-while IFS= read -rd '' f; do
-    # Exclude some files.
-    if [[ "$f" == "thirdparty"* ]]; then
+    # Fix copyright headers, but not all files get them.
+    if [[ "$f" == *"inc" ]]; then
+        continue
+    elif [[ "$f" == *"glsl" ]]; then
+        continue
+    elif [[ "$f" == "platform/android/java/lib/src/org/godotengine/godot/input/InputManager"* ]]; then
         continue
     fi
 
-    for extension in ${CLANG_FORMAT_FILE_EXTS[@]}; do
-        if [[ "$f" == *"$extension" ]]; then
-            # Run clang-format.
-            clang-format -i "$f"
-            continue 2
-        fi
-    done
+    python misc/scripts/copyright_headers.py "$f"
 done
 
-git diff > patch.patch
+diff=$(git diff --color)
 
 # If no patch has been generated all is OK, clean up, and exit.
-if [ ! -s patch.patch ] ; then
-    printf "Files in this commit comply with the clang-format style rules.\n"
-    rm -f patch.patch
+if [ -z "$diff" ] ; then
+    printf "Files in this commit comply with the clang-tidy style rules.\n"
     exit 0
 fi
 
 # A patch has been created, notify the user, clean up, and exit.
-printf "\n*** The following differences were found between the code "
-printf "and the formatting rules:\n\n"
-cat patch.patch
-printf "\n*** Aborting, please fix your commit(s) with 'git commit --amend' or 'git rebase -i <hash>'\n"
-rm -f patch.patch
+printf "\n*** The following changes have been made to comply with the formatting rules:\n\n"
+echo "$diff"
+printf "\n*** Please fix your commit(s) with 'git commit --amend' or 'git rebase -i <hash>'\n"
 exit 1
