c6f7a72e07aaa3a776c3433c434b6e09de5dc315
oidro <31112680+oidro@users.noreply.github.com>
2022-04-28 16:30:29 -0400

    Unified & improved build script

--- sourcecode/chunkgen/buildWindowsRelease.bat
+++
@@ -1,3 +0,0 @@
-cargo build --release
-copy target\release\chunkgeneration.dll ..\VoxelGame\bins\libchunkgeneration.dll
-copy target\release\chunkgeneration.dll ..\VoxelGameServer\bins\libchunkgeneration.dll
