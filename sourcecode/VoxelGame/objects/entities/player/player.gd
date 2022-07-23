f4ed67cc1798e02f51a45d6d71cc1006bc03b953
Cherry <staticdroidpop@gmail.com>
2022-07-22 22:42:05 -0400

    Server and client prep

--- sourcecode/VoxelGame/objects/entities/player/player.gd
+++ sourcecode/VoxelGame/objects/entities/player/player.gd
@@ -1,11 +1,5 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# this'll handle how the server the executable will boot,
+# if its a server it'll only boot as the server within a terminal,
+# while if its the client we'll preload any assets, textures, whatever is required, etc.
