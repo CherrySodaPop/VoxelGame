a1b0e030d77cea5555ad1ab8ae3c57b0ac37d185
Cherry <staticdroidpop@gmail.com>
2022-07-25 18:35:45 -0400

    Player model

--- sourcecode/VoxelGame/objects/entities/entityPlayer/entityPlayer.gd
+++ sourcecode/VoxelGame/objects/entities/entityPlayer/entityPlayer.gd
@@ -1,5 +1,1 @@
 extends Node3D
-
-# this'll handle how the server the executable will boot,
-# if its a server it'll only boot as the server within a terminal,
-# while if its the client we'll preload any assets, textures, whatever is required, etc.
